4515ec31584a3e22ffac403926a71b2021c170ef
Anderson Zanichelli <anderzanic@gmail.com>
2016-02-09 15:37:29 -0200

    Implementação do projeto

---
+++ randomicWeather.js
@@ -0,0 +1,75 @@
+module.exports = {
+
+	now: function(provider) {
+		var weatherInformation = [];
+		var cities = ["Curitiba", "Maringá", "Londrina", "Foz do Iguaçu", "Guarapuava"];
+		var time = {
+			HOUR: 23,
+			MINUTES: 59,
+			SECONDS: 59,
+			MILLIS: 999
+		};
+
+		var sky = function(temperature) {
+			var situation = ["weather-snowy", "weather-rainy", "weather-stormy", "weather-cloudy", "weather-cloudy-sunny", "weather-sunny"];
+			if(temperature > 0 && (temperature % 6) == 0)
+				return situation[temperature / 6]
+		
+			return temperature > 0 ? situation[temperature % 6] : situation[0];
+		};
+
+		var conditions = function(){
+			var positive = Math.floor((Math.random() * 36) + 1);
+			var negative = Math.floor((Math.random() * 6) + 1);
+			var humidity = Math.floor((Math.random() * 100) + 1);
+			var temperature = positive - negative;
+
+			var condition = {"temperature": temperature, "humidity": humidity}
+
+			return condition;
+		};
+
+		var today = new Date();
+
+		var randonTime = function(limit) {
+			var now = 0;
+
+			switch(limit){
+				case time.HOUR:
+					now = today.getHours();
+					break;
+				case time.MINUTES:
+					now = today.getMinutes();
+					break;
+				case time.SECONDS:
+					now = today.getSeconds();
+					break;
+				default:
+					now = 0;
+			}
+
+			var rand = Math.floor((Math.random() * limit));
+
+			while(rand > now)
+				rand = Math.floor((Math.random() * limit));
+
+			return rand;
+		};
+
+		for (var i = 0; i < cities.length; i++) {
+			var condition = conditions();
+			var info = {
+				"provider": provider,
+				"city": cities[i],
+				"temperature": condition.temperature,
+				"Humidity": condition.humidity,
+				"sky": sky(condition.temperature),
+				"update": new Date(today.getFullYear(), today.getMonth(), today.getDate(), randonTime(time.HOUR), randonTime(time.MINUTES), randonTime(time.SECONDS), Math.floor((Math.random() * 999)))
+			};
+
+			weatherInformation.push(info);
+		}
+		
+		return weatherInformation;
+	}
+}
---
+++ server.js
@@ -0,0 +1,32 @@
+var restify = require('restify');
+var randomicWeather = require('./randomicWeather.js');
+
+var server = restify.createServer();
+var port = process.env.PORT || 9003;
+process.env.TZ = 'Brazil/East';
+
+server.use(restify.bodyParser({ mapParams: true }));
+
+var weather = {};
+
+weather.provider = function() {
+	return "Third Weather Provider";
+};
+
+weather.hello = function(req, res, next) {
+	res.send('Hello, this is the third weather provider' + req.params['param'] + '!');
+	next();
+};
+
+weather.now = function(req, res, next) {
+	res.json(randomicWeather.now(weather.provider()));
+	next();
+};
+
+
+server.get('/hello/:param', weather.hello);
+server.get('/weather', weather.now);
+
+server.listen(port, function() {
+  console.log('%s listening at server port %s', 'Third Weather Info', port);
+});
